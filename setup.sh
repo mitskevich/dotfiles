d5181e438755de36a389d36ebdfe39b65b59b6e3
Sergey Mitskevich <sergey.mitskevich@overvis.com>
2022-10-21 12:29:05 +0000

    init.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+ln -svf ~/dotfiles/.profile ~
+ln -svf ~/dotfiles/.bashrc ~
+ln -svf ~/dotfiles/.bash_logout ~
+ln -svf ~/dotfiles/.gitconfig ~
+ln -svf ~/dotfiles/.gitignore_global ~
